16a7f9177532f717615df93355d8787cdc82fc6f
FlyinPancake <36113055+FlyinPancake@users.noreply.github.com>
2026-05-10 22:51:15 +0200

    fix(frontend): fix a bug where the main app page could 404

--- frontend/src/components/Stats/Stats.tsx
+++ frontend/src/components/Stats/Stats.tsx
@@ -2,23 +2,24 @@ import { getStats } from "/src/app/actions";
 import { useTranslation } from "/src/i18n/server";
 
 import styles from "./Stats.module.scss";
-import { notFound } from "next/navigation";
 
 const Stats = async () => {
   const statsResponse = await getStats();
-  if (!statsResponse.ok) return notFound();
-  const stats = statsResponse.data;
 
   const { t } = await useTranslation("home");
 
-  return stats ? (
+  return statsResponse.ok ? (
     <div className={styles.wrapper}>
       <div>
-        <span className={styles.number}>{new Intl.NumberFormat().format(stats.event_count)}</span>
+        <span className={styles.number}>
+          {new Intl.NumberFormat().format(statsResponse.data.event_count)}
+        </span>
         <span className={styles.label}>{t("about.events")}</span>
       </div>
       <div>
-        <span className={styles.number}>{new Intl.NumberFormat().format(stats.person_count)}</span>
+        <span className={styles.number}>
+          {new Intl.NumberFormat().format(statsResponse.data.person_count)}
+        </span>
         <span className={styles.label}>{t("about.availabilities")}</span>
       </div>
     </div>
